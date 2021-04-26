b7a340607e7df73718f810c6e2d6ffcd6e7ab91b
tshuna332 <tmmi333gmail.com>
2021-04-26 14:20:14 +0000

    the ej*

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+
+	current = list;
+
+	for (; 1 == 1;)
+	{
+		current = current->next;
+
+		if (current == NULL)
+			return (0);
+		else if (current == list)
+			return (1);
+	}
+
+	return (0);
+}
