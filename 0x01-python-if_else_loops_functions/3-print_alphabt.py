ee0b4aa8d432bb41f042ce33833bdb88ad94b188
tshuna332 <tmmi333gmail.com>
2021-04-27 16:17:18 +0000

    fix error

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')):
+for i in range(97, 123):
     if chr(i) not in "qe":
-        print(chr(i), end='')
+        print("{}".format(chr(i)), end='')
