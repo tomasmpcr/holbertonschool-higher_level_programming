57a797c4a1a6d5bf92f49930a817d03809d28b0f
tshuna332 <tmmi333gmail.com>
2021-04-27 15:44:03 +0000

    fix ej 1, change variable to a int

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number = str(number)
-last_digit = number[-1:]
-if number[0] == "-" and last_digit != "0":
-    last_digit = "-" + last_digit
-print("Last digit of {} is {} ".format(number, last_digit), end="")
-if last_digit == "0":
+last_digit = 1
+r_number = number
+if number < 0:
+    last_digit = last_digit * -1
+    r_number = r_number * -1
+last_digit = (r_number % 10) * last_digit
+print("Last digit of {:d} is {:d} ".format(number, last_digit), end="")
+if last_digit == 0:
     print("and is 0")
-elif last_digit < "6":
+elif last_digit < 6:
     print("and is less than 6 and not 0")
 else:
     print("and is greater than 5")
+
