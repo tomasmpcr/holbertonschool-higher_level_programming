c2370f071ac4fbc173683bb098077acc71af7cac
mstom332 <tomas@332.com.uy>
2021-04-29 16:27:53 +0000

    fix ej 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     x = dir("hidden_4.pyc")
+    x.sort()
     for i in range(0, len(x)):
         if x[i][:2] != "__":
             print("{}".format(x[i]))
