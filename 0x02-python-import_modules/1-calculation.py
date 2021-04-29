aed0135e46fac79e13883312e635a685547d8a5a
mstom332 <tomas@332.com.uy>
2021-04-29 15:21:49 +0000

    fix ej 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-def _print(a, s, b, r):
-    print("{:d} {} {:d} = {:d}".format(a, s, b, r))
-
 if __name__ == "__main__":
-    from calculator_1 import add
-    from calculator_1 import sub
-    from calculator_1 import mul
-    from calculator_1 import div
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    _print(a, "+", b, add(a, b))
-    _print(a, "-", b, sub(a, b))
-    _print(a, "*", b, mul(a, b))
-    _print(a, "/", b, div(a, b))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
