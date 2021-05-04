794a69f839d1d7b3a002674fca68ba4f44b6bcd5
mstom332 <tomas@332.com.uy>
2021-05-04 20:07:08 +0000

    add ej 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int _strlist(listint_t *head);
+int _get_n_inde_list(listint_t *head, int index);
+
+/**
+* is_palindrome - check is palindrome
+* @head: the pinter
+* -----------------------------------------------
+* Return int
+*/
+int is_palindrome(listint_t **head)
+{
+    int len_list = _strlist(*head);
+    int i, j = len_list - 1;
+    int paso = 1;
+
+    if (head == NULL || len_list == 0)
+        return (1);
+
+    for (i = 0; i < (len_list / 2); i++, j--)
+    {
+        if (_get_n_inde_list(*head, i) != _get_n_inde_list(*head, j))
+        {
+            paso = 0;
+        }
+    }
+
+    return (paso);
+}
+
+int _get_n_inde_list(listint_t *head, int index)
+{
+    int i;
+
+    if (head == NULL)
+        return (-1);
+
+    for (i = 0; head; i++)
+    {
+        if (i == index)
+            return (head->n);
+
+        head = head->next;
+    }
+
+    return (-1);
+}
+
+int _strlist(listint_t *head)
+{
+    int i;
+
+    if (head == NULL)
+        return (0);
+
+    for (i = 0; head; i++)
+    {
+        head = head->next;
+    }
+    return (i);
+}
