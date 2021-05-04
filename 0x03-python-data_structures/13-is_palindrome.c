392854c09f23631eab3ec74743a3d0e73a4455e9
mstom332 <tomas@332.com.uy>
2021-05-04 20:50:52 +0000

    add ej 13 fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,75 +2,48 @@
 #include <stdlib.h>
 #include "lists.h"
 
-int _strlist(listint_t *head);
-int _get_n_inde_list(listint_t *head, int index);
+listint_t *reverse_list(listint_t **head);
 
 /**
 * is_palindrome - check is palindrome
 * @head: the pinter
 * -----------------------------------------------
-* Return: int
+* Return int
 */
 int is_palindrome(listint_t **head)
 {
-	int len_list = _strlist(*head);
-	int i, j = len_list - 1;
+	listint_t *rev_list = NULL;
 
-	if (head == NULL || len_list == 0)
+	if (head == NULL || *head == NULL)
 		return (1);
 
-	for (i = 0; i < (len_list / 2); i++, j--)
+	rev_list = reverse_list(head);
+
+	for (; *head && rev_list;)
 	{
-		if (_get_n_inde_list(*head, i) != _get_n_inde_list(*head, j))
-		{
+		if ((*head)->n != rev_list->n)
 			return (0);
-		}
+
+		*head = (*head)->next;
+		rev_list = rev_list->next;
 	}
 
 	return (1);
 }
 
-/**
-* _get_n_inde_list - check is palindrome
-* @head: the pinter
-* @index: the pinter
-* -----------------------------------------------
-* Return: int
-*/
-int _get_n_inde_list(listint_t *head, int index)
+listint_t *reverse_list(listint_t **head)
 {
-	int i;
+	listint_t *next = NULL;
+	listint_t *current = *head;
+	listint_t *prev = NULL;
 
-	if (head == NULL)
-		return (-1);
-
-	for (i = 0; head; i++)
+	for (; current;)
 	{
-		if (i == index)
-			return (head->n);
-
-		head = head->next;
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
 	}
 
-	return (-1);
-}
-
-/**
-* _strlist - check is palindrome
-* @head: the pinter
-* -----------------------------------------------
-* Return: int
-*/
-int _strlist(listint_t *head)
-{
-	int i;
-
-	if (head == NULL)
-		return (0);
-
-	for (i = 0; head; i++)
-	{
-		head = head->next;
-	}
-	return (i);
+	return (prev);
 }
