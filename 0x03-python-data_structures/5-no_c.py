8efb91f9daee49cb7653c62c2f0268354e9cb1f3
mstom332 <tomas@332.com.uy>
2021-05-04 18:25:30 +0000

    fix ej 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     str_new = ""
     for i in range(0, len(my_string)):
-        if my_string[i] != "c" and my_string[i] != "C":
+        if my_string[i] != 'c' and my_string[i] != 'C':
             str_new += my_string[i]
     return (str_new)
