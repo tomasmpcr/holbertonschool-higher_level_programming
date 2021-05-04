b7e2f7a5df05631d8290df6b4580213f32d5b8db
mstom332 <tomas@332.com.uy>
2021-05-04 19:15:38 +0000

    fix ej 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return ((len(sentence), sentence[0]))
+    ret = (len(sentence), None)
+    if sentence:
+        ret = (len(sentence), sentence[0])
+    return (ret)
