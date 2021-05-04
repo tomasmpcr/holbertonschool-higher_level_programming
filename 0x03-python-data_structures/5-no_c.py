e6a6746fd1d2c51767f76e772fa4e1b67c35ba50
mstom332 <tomas@332.com.uy>
2021-05-04 18:14:43 +0000

    fix ej 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    my_list = list(my_string)
-    index_remove = []
-    for i in range(0, len(my_list)):
-        if (my_list[i] == "c" or my_list[i] == "C"):
-            index_remove.append(i)
-
-    for i in range(0, len(index_remove)):
-        del my_list[index_remove[i]]
-    cpy_string = ''.join(my_list)
+    cpy_string = my_string.translate({ord(i): None for i in 'cC'})
     return (cpy_string)
