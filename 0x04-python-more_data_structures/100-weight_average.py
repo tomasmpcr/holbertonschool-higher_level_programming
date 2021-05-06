5f41f95bb060ccc3a787c2864c93fb5374010c7a
mstom332 <tomas@332.com.uy>
2021-05-06 22:39:35 +0000

    adde ej 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    total = 0
+    div = 0
+    for tup in my_list:
+        total += tup[0] * tup[1]
+        div += tup[1]
+    return (total / div)
