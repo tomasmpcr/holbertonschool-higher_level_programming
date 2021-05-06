1ab9cf8a6a6f7203b089de8e3cfa37bd4ef19e92
mstom332 <tomas@332.com.uy>
2021-05-06 16:48:36 +0000

    fix ej 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
-        return
-    return (max(a_dictionary))
+        return (None)
+    key_max = None
+    value_max = 0
+
+    for (key, value) in a_dictionary.items():
+        if value > value_max:
+            key_max = key
+            value_max = value
+
+    return (key_max)
