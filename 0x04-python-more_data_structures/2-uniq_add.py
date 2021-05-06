4575964ad023c61e6c229d69b90dbba11607d957
mstom332 <tomas@332.com.uy>
2021-05-06 15:18:39 +0000

    fix ej 2, and add ej 3

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    return (0)
+    new_list = set(my_list)
+    suma = 0
+    for i in new_list:
+        suma += i
+    return (suma)
---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    for i in range(0, len(set_1)):
+        return (0)
