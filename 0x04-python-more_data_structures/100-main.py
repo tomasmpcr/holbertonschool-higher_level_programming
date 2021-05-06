b4fae0d1fd7647c0713d5dbd8891fe4a0b8e2ee6
mstom332 <tomas@332.com.uy>
2021-05-06 22:44:03 +0000

    fix ej 100

--- 0x04-python-more_data_structures/100-main.py
+++ 0x04-python-more_data_structures/100-main.py
@@ -3,5 +3,5 @@ weight_average = __import__('100-weight_average').weight_average
 
 my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
 # = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
-result = weight_average(my_list)
+result = weight_average()
 print("Average: {:0.2f}".format(result))
--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,10 @@
 def weight_average(my_list=[]):
     total = 0
     div = 0
+
+    if len(my_list) == 0:
+        return (0)
+
     for tup in my_list:
         total += tup[0] * tup[1]
         div += tup[1]
