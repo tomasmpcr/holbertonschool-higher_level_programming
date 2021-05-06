9c520dc274ef933e4d8838742ec3484a2c168439
mstom332 <tomas@332.com.uy>
2021-05-06 20:50:33 +0000

    add ej 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    suma = 0
+
+    for i in range(0, len(roman_string)):
+        if i + 1 < len(roman_string):
+            if roman_value(roman_string[i]) < roman_value(roman_string[i + 1]):
+                suma -= roman_value(roman_string[i])
+                continue
+        suma += roman_value(roman_string[i])
+    return (suma)
+
+
+def roman_value(letter):
+    switcher = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    return (switcher.get(letter, None))
