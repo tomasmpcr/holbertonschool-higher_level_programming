5b42c11ee9ebd84a132543305d2b5e5675f1882f
mstom332 <tomas@332.com.uy>
2021-05-06 15:04:57 +0000

    fix ej 1 and add ej 2

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     new_list = []
     for i in range(0, len(my_list)):
-        if i == search:
+        if i == search - 1:
             new_list.append(replace)
         else:
             new_list.append(my_list[i])
