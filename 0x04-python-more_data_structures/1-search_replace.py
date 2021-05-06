ada1284215ebf975b9e4785963de2ff63e82d2ff
mstom332 <tomas@332.com.uy>
2021-05-06 15:10:41 +0000

    fix ej 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    if my_list is None:
-        return (new_list)
     for i in range(0, len(my_list)):
-        if i == search - 1:
+        if my_list[i] == search:
             new_list.append(replace)
         else:
             new_list.append(my_list[i])
