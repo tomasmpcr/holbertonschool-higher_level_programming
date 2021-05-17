6a46ed7e6613a6eadf01cc0b7273df376322b6c9
mstom332 <tomas@332.com.uy>
2021-05-17 16:18:40 +0000

    fix ej 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(0, x+1):
+    imp = 0
+    for i in range(0, x):
         try:
             print(my_list[i], end="")
+            imp = imp + 1
         except:
             break
     print("")
-    return (i)
+    return (imp)
