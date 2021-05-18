c034b0878e709593ec210571060b86670e287990
mstom332 <tomas@332.com.uy>
2021-05-18 19:32:09 +0000

    add __doc__ ej 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""In this module you will find a class
+"Square" at the moment it does nothing"""
+
+
 class Square:
+    """In this class we save a data"""
     __size = 0
 
     def __init__(self, size):
+        """This is the constructor of the class"""
         self.__size = size
         return
