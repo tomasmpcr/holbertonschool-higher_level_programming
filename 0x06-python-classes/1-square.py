b326bed0a18435f9cf0975c2b70f3a036cd25d28
mstom332 <tomas@332.com.uy>
2021-05-18 15:34:10 +0000

    add ej n

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    __size = 0
+
+    def __init__(self, size):
+        self.__size = size
+        return
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+class Square:
+    __size = 0
+
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
