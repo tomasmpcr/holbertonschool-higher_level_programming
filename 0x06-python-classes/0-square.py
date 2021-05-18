71a08a23e055a305626012295e6d75eb863e9bdf
mstom332 <tomas@332.com.uy>
2021-05-18 19:29:00 +0000

    add __doc__

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""In this module you will find a class
+"Square" at the moment it does nothing"""
+
+
 class Square:
+    """For now in this class we have nothing"""
+
     def __init__(self):
+        """This is the constructor of the class"""
         return
