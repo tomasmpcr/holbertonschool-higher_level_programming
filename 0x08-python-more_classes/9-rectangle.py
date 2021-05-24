4b1c3cd14301924aa1fa4efbba1e4a6c6e3397ad
mstom332 <tomas@332.com.uy>
2021-05-24 22:26:57 +0000

    fix ej 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -107,9 +107,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         elif type(rect_2) != Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        elif rect_1.area() == rect_2.area():
-            return (rect_1)
-        elif rect_1.area() > rect_2.area():
+        elif rect_1.area() >= rect_2.area():
             return (rect_1)
         else:
             return (rect_2)
