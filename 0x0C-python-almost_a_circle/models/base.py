753c77c90cf1f39b6408f7a699f42d5926d311b2
mstom332 <tomas@332.com.uy>
2021-06-10 19:18:57 +0000

    add ej 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,3 +26,14 @@ class Base():
         if list_dictionaries is None:
             return("[]")
         return(json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Funcion """
+        new_list = []
+        if list_objs is not None:
+            for element in list_objs:
+                new_list.append(element.to_dictionary())
+        with open(cls.__name__ + ".json", "+w") as file:
+            file.write(cls.to_json_string(new_list))
+        return
