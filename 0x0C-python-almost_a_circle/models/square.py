15e28e3c4a6301c762c9a305d7ff599670dfcaea
mstom332 <tomas@332.com.uy>
2021-06-10 15:57:43 +0000

    add ej 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Class Square
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Class square """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Funcion init """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ Funcion """
+        return("[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id, self.x, self.y, self.height
+        ))
