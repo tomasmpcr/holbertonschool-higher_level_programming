546f59f8585a1b48260de1bf8a6eb9af329c2ccb
mstom332 <tomas@332.com.uy>
2021-06-10 16:30:40 +0000

    add ej 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,45 @@ class Square(Rectangle):
         self.width = value
         self.height = value
         return
+
+    def update(self, *args, **kwargs):
+        """ Funcion """
+        switcher = {
+            1:
+                lambda value: [None for self.id in [value]],
+            "id":
+                lambda value: [None for self.id in [value]],
+            2:
+                lambda value:
+                    [[None for self.width in [value]],
+                        [None for self.height in [value]]],
+            "size":
+                lambda value:
+                    [[None for self.width in [value]],
+                        [None for self.height in [value]]],
+            3:
+                lambda value: [None for self.x in [value]],
+            "x":
+                lambda value: [None for self.x in [value]],
+            4:
+                lambda value: [None for self.y in [value]],
+            "y":
+                lambda value: [None for self.y in [value]]
+        }
+
+        if args is not None and len(args) > 0:
+            i = 1
+            for arg in args:
+                func = switcher.get(i, "return")
+                if func == "return":
+                    return
+                func(arg)
+                i = i + 1
+            return
+
+        for item in kwargs.items():
+            func = switcher.get(item[0], "return")
+            if func == "return":
+                return
+            func(item[1])
+        return
