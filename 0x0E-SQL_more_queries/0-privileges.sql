7bbf364397a055261d528bfa41e841b8e3c6e899
mstom332 <tomas@332.com.uy>
2021-07-13 17:24:40 +0000

    add

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
--- Show permissions for user_0d_1
+-- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- on your server (in localhost).
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- Show permissions for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
