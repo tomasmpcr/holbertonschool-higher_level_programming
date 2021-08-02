efa964a5d5a35a1b9d0d3a64eaa0204aef77a1c4
mstom332 <tomas@332.com.uy>
2021-08-02 19:17:03 +0000

    add ej 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const { argv } = require('process');
+
+if (parseInt(argv[2])) {
+  for (let i = 0; i < parseInt(argv[2]); i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
