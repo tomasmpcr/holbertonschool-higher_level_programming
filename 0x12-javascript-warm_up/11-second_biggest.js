ca1e931e59f6ec9fa42ff708a4fdabaab1995c6f
mstom332 <tomas@332.com.uy>
2021-08-02 23:46:39 +0000

    add

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const { argv } = require('process');
+
+argv.splice(0, 2);
+
+if (argv.length <= 1) {
+  console.log(1);
+} else {
+  const newArray = argv.sort(function (a, b) {
+    return (a - b);
+  });
+
+  console.log(newArray[newArray.length - 2]);
+}
