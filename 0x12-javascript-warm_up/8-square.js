69797a8e236f0f5e4ef821397c266f6bd903b6c7
mstom332 <tomas@332.com.uy>
2021-08-02 19:21:45 +0000

    add ej 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const { argv } = require('process');
+
+if (parseInt(argv[2])) {
+  for (let i = 0; i < parseInt(argv[2]); i++) {
+    let cadena = '';
+    for (let j = 0; j < parseInt(argv[2]); j++) {
+      cadena += 'X';
+    }
+    console.log(cadena);
+  }
+} else {
+  console.log('Missing size');
+}
