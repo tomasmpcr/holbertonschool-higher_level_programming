a0b57b059d7086cf05c09127652b9f6546797051
mstom332 <tomas@332.com.uy>
2021-08-02 18:53:35 +0000

    add ej 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const { argv } = require('process');
+
+if (argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  for (let i = 2; argv[i] != undefined; i++) {
+    console.log(argv[i]);
+  }
+}
