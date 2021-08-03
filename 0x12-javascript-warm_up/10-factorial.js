f233f784e84335d431f37879d39a5fc08283a07e
mstom332 <tomas@332.com.uy>
2021-08-03 00:19:56 +0000

    add ej 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,9 +3,9 @@ const { argv } = require('process');
 
 console.log(fact(parseInt(argv[2])));
 
-function fact (numMax, i = 1) {
-  if (isNaN(numMax) || numMax <= i) {
-    return (i);
+function fact (num) {
+  if (num === 0) {
+    return 1;
   }
-  return (fact(numMax, i + 1) * i);
+  return (num * fact(num - 1));
 }
