1a9e04649cfa4cffbf305400bc6538a67a8922b4
mstom332 <tomas@332.com.uy>
2021-08-08 15:24:17 +0000

    add

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    miConexion = MySQLdb.connect(host='localhost',
+                                 user=argv[1],
+                                 passwd=argv[2],
+                                 db=argv[3])
+
+    cur = miConexion.cursor()
+    cur.execute("SELECT * FROM states")
+
+    for row in cur.fetchall():
+        print(row)
+
+    miConexion.close()
