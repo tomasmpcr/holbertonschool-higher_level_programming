f9ee17d3a71288c94ffe03ea0390499133e28047
mstom332 <tomas@332.com.uy>
2021-08-21 00:27:02 +0000

    asd

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,5 +9,7 @@ if __name__ != "__main__":
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     status = response.read()
     decode_response = status.decode('utf-8')
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: \
-{}".format(type(status), status, decode_response))
+    print("Body response:")
+    print("\t - type: {}".format(type(status)))
+    print("\t - content: {}".format(status))
+    print("\t - utf8 content: {}".format(decode_response))
