ce1fe2b50fa36d9820e587d25dee9113e3bf2f52
tshuna332 <tmmi333@gmail.com>
2021-09-14 20:53:26 +0000

    asd

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+
+request(options, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    try {
+      fs.writeFile('loripsum', body, 'utf8', function (err, data) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
