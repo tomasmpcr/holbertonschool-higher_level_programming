9972a58d7bdb574aee866c05e8f7cd7ef779dc3b
tshuna332 <tmmi333@gmail.com>
2021-09-14 20:47:39 +0000

    asd

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+
+request(options, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let count = 0;
+    const obj = JSON.parse(body);
+    obj.results.forEach(element => {
+      element.characters.forEach(element2 => {
+        if (element2 === 'https://swapi-api.hbtn.io/api/people/18/') {
+          count++;
+        }
+      });
+    });
+    console.log(count);
+  }
+});
