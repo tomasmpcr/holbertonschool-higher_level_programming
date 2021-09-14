1bfa8cbe4498d046bf10ebf0c1af072bf7b1a5de
tshuna332 <tmmi333@gmail.com>
2021-09-14 19:38:22 +0000

    asd

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 
 try {
-  fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err,data) {
+  fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, data) {
     if (err) {
       return console.log(err);
     }
