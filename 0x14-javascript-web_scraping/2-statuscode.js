5e4f70ed5a12a73f9146e1af71bd3d5341658b29
tshuna332 <tmmi333@gmail.com>
2021-09-14 20:20:02 +0000

    asd

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-
 const options = {
-    url: process.argv[2],
-    method: 'GET'
+  url: process.argv[2],
+  method: 'GET'
 };
 
-request(options, function(err, res, body) {
-    console.log('code: '+res.statusCode);
+request(options, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + res.statusCode);
+  }
 });
