256c39780e15a63d00a0fa0816ac4a7528984cfd
tshuna332 <tmmi333@gmail.com>
2021-10-26 16:49:17 +0000

    asd

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-""" ASDASDASDASDASDADSASDASD """
+""" asdadshfuhdsafh dsfhds """
 
 if __name__ == "__main__":
-
     from sys import argv
     import MySQLdb
-    MY_H = "localhost"
-    MY_U = argv[1]
-    MY_P = argv[2]
-    MY_D = argv[3]
-    nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
-    consulta = nuevaconexion.cursor()
-    texto1 = "SELECT c.id, c.name, s.name FROM"
-    texto2 = " cities c INNER JOIN states s ON c.state_id = s.id"
-    consulta.execute(texto1 + texto2)
-    resultado = consulta.fetchall()
+
+    conn = MySQLdb.connect(host="localhost", user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    query = conn.cursor()
+    sql = "SELECT c.id, c.name, s.name FROM cities c INNER JOIN"
+    sql += " states s ON c.state_id = s.id ORDER BY c.id"
+    query.execute(sql)
+    resultado = query.fetchall()
     for fila in resultado:
         print(fila)
-    nuevaconexion.close()
+    conn.close()
