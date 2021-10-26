ff5cc456a30f50e6ef9bd8bcaa013446c996a2dc
tshuna332 <tmmi333@gmail.com>
2021-10-26 16:32:35 +0000

    asd

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" asdadshfuhdsafh dsfhds """
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    conn = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+    query = conn.cursor()
+    sql = "SELECT c.id, c.name, s.name FROM"
+    sql += " cities c INNER JOIN states s ON c.state_id = s.id"
+    query.execute(sql)
+    resultado = query.fetchall()
+    for fila in resultado:
+        print(fila)
+    conn.close()
