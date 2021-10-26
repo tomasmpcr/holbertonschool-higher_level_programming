4e49b2ee8dee0228b3f1702442e9e87476003c16
tshuna332 <tmmi333@gmail.com>
2021-10-26 18:32:05 +0000

    asd

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""ASASDSDFGSDF JSDIFJDS IFJDSFDSF"""
+
+import requests
+
+if __name__ == "__main__":
+    url = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(url.text)))
+    print("\t- content: {}".format(url.content.decode('utf-8')))
